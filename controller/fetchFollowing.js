b619ec855bd5dad3c5e81fb9e2d90684e3f5ad9e
Pratik Sah <7805783-pratiksah@users.noreply.gitlab.com>
2021-06-11 15:48:28 +0530

    Added sequelize

--- controller/fetchFollowing.js
+++ controller/fetchFollowing.js
@@ -1,16 +1,19 @@
 // import {Sequelize, QueryTypes} from "sequelize";
 import dash from "../helpers/logger.js";
+import pkg from 'sequelize'
+const { QueryTypes } = pkg;
+import conn from '../db/db-connect.js'
 
 const fetchFollowing = async (req, res) => {
   try {
 
-    // const following = await Sequelize.query(
-    //   "SELECT users.fullName, users.userName, users.profileImage FROM followers INNER JOIN users ON followers.following = users.id WHERE followers.userId = ?",
-    //   {
-    //     type: QueryTypes.SELECT,
-    //     replacements: [req.id],
-    //   }
-    // );
+    const following = await conn.query(
+      "SELECT users.fullName, users.userName, users.profileImage FROM followers INNER JOIN users ON followers.following = users.id WHERE followers.userId = ?",
+      {
+        type: QueryTypes.SELECT,
+        replacements: [req.id],
+      }
+    );
 
     res.status(201).json({
       msg: "Followed successfully",
